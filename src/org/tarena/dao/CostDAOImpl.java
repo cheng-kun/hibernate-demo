feaede3096757df6c4856adf5cc9de82a4491270
cheng-kun <chengkunche@yahoo.com>
2019-05-20 17:07:16 -0400

    hibernateUtil

--- src/org/tarena/dao/CostDAOImpl.java
+++ src/org/tarena/dao/CostDAOImpl.java
@@ -1,23 +1,16 @@
 package org.tarena.dao;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+
 import org.tarena.entity.Cost;
 
 public class CostDAOImpl implements CostDAO {
 
 	public Cost findById(int id) {
 		
-		Configuration conf = new Configuration();
-		conf.configure("/hibernate.cfg.xml");
+		Session session = HibernateUtil.openSession();
 		
-		SessionFactory sFactory = conf.buildSessionFactory();
-		
-		
-		Session session = sFactory.openSession();
-		
-		Cost cost =  session.get(Cost.class, id);
+		Cost cost =  (Cost)session.get(Cost.class, id);
 		
 		return cost;
 	}
